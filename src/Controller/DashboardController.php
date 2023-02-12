4bc09d3989129d1799f62583b44a0dc501465846
Achref <achref.maarfi0@gmail.com>
2023-02-12 16:33:47 +0100

    fixing names

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -6,19 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DashboardController extends AbstractController
+class BackOfficeController extends AbstractController
 {
-    #[Route('/dashboard', name: 'app_dashboard')]
+    #[Route('/back', name: 'app_back')]
     public function index(): Response
     {
-        return $this->render('dashboard/admin.html.twig', [
+        return $this->render('back_office/back.html.twig', [
             'controller_name' => 'DashboardController',
         ]);
     }
     #[Route('/users', name: 'app_users')]
     public function users_list(): Response
     {
-        return $this->render('dashboard/users.html.twig', [
+        return $this->render('back_office/back.html.twig', [
             'controller_name' => 'DashboardController',
         ]);
     }
