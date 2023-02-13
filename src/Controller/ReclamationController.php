e228a0c8df9d177cafa449e81352492aa218bc7c
tass26 <taycir.hajjii@gmail.com>
2023-02-13 22:32:39 +0100

    fist

--- src/Controller/ReclamationController.php
+++ src/Controller/ReclamationController.php
@@ -79,6 +79,7 @@ class ReclamationController extends AbstractController
             $entityManager->flush();
         }
 
-           return $this->redirectToRoute('app_reclamation_index', [], Response::HTTP_SEE_OTHER);
+
+        return $this->redirectToRoute('app_reclamation_index', [], Response::HTTP_SEE_OTHER);
   }
 }
